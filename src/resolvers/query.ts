2ff3b3293c915bd204623a1a210c1f8bb307d9ad
Kevin <kevin.lam13@hotmail.com>
2021-09-12 18:33:52 -0600

    first commit

---
+++ src/resolvers/query.ts
@@ -0,0 +1,45 @@
+import { IResolvers } from 'graphql-tools';
+import { database } from '../data/data.store';
+
+const query: IResolvers = {
+    Query: {
+        estudiantes(): any {
+            return database.estudiantes;
+        },
+        estudiante(__: void, {id}): any {
+            const result = database.estudiantes.filter(estudiante => estudiante.id === id)[0];
+            if(result === undefined){
+                return {
+                    id: '-1',
+                    name: `No se ha encontrado el estudiante con el ID ${id}`,
+                    email: '',
+                    courses: []
+                }
+            }
+            return result;
+        },
+        cursos(): any {
+            return database.cursos;
+        },
+        curso(__: void, {id}): any {
+            const result = database.cursos.filter(curso => curso.id === id)[0];
+            if(result === undefined){
+                return {
+                    id: '-1',
+                    title: `No se ha encontrado el curso con el ID ${id}`,
+                    description: '',
+                    clases: -1,
+                    time: 0.0,
+                    logo: '',
+                    level: "TODOS",
+                    path: '',
+                    teacher: '',
+                    reviews: []
+                }
+            }
+            return result;
+        },
+    }
+}
+
+export default query;
---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import { createServer } from 'http';
+import { ApolloServer } from 'apollo-server-express';
+import expressPlayGround from 'graphql-playground-middleware-express';
+
+import schema from './schema/index';
+
+
+const app = express();
+
+app.use(cors());
+app.use(compression());
+
+const servidor = new ApolloServer({
+    schema,
+    introspection: true
+});
+
+servidor.applyMiddleware({app});
+
+app.get('/',expressPlayGround({
+    endpoint: '/graphql'
+}));
+
+const httpServer = createServer(app);
+
+const PORT = 5200;
+
+httpServer.listen(PORT, () => {
+    console.log(`Servidor academia online conectado http://localhost:${PORT}`);
+});
